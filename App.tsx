12746857a166b372b09b522853c10585512315a9
jonatasJS <soaresjonatas398@gmail.com>
2022-08-29 19:19:23 -0300

    asdasdd

--- App.tsx
+++ App.tsx
@@ -1,6 +1,70 @@
 import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, AsyncStorage, Alert } from 'react-native';
+
+export default function App() {
+  const [copos, setCopos] = useState(Number(AsyncStorage.getItem('copos')) || 0);
+
+  useEffect(() => {
+    setCopos(Number(AsyncStorage.getItem('copos')) || 0);
+  }, [])
+
+  async function addCopo() {
+    try {
+      setCopos(copos + 1);
+      await AsyncStorage.setItem('copos', String(copos + 1));
+    } catch {
+      Alert.alert('Erro', 'Erro ao adicionar copo');
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Bebi "{copos}" copos de agua.</Text>
+      <Text
+        style={styles.button}
+        accessibilityLabel="Beber"
+        onPress={addCopo}
+      >{"Beber"}</Text>
+      <Text
+        style={styles.button}
+        accessibilityLabel="Limpar"
+        onPress={() => setCopos(0)}
+      >Zerar</Text>
+      <StatusBar style="light" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#2d3437',
+    alignItems: 'center',
+    justifyContent: 'center',
+    display: 'flex'
+  },
+  text: {
+    fontSize: 30,
+    color: "#fff"
+  },
+  button: {
+    fontSize: 30,
+    margin: 50,
+    paddingBottom: 20,
+    paddingTop: 20,
+    paddingLeft: 40,
+    paddingRight: 40,
+    color: "#fff",
+    alignItems: 'center',
+    justifyContent: 'center',
+    display: 'flex',
+    backgroundColor: '#1f9ace'
+  }
+});
+
+/*
+
 import moment from 'moment';
 import {
   titleTimer,
@@ -46,23 +110,9 @@ export default function App() {
       {hoursTimer.indexOf(new Date().getSeconds()) != -1 && <Text style={styles.text}>falta {hours} horas</Text>}
       {minutesTimer.indexOf(new Date().getSeconds()) != -1 && <Text style={styles.text}>falta {minutes} minutos</Text>}
       {secondsTimer.indexOf(new Date().getSeconds()) != -1 && <Text style={styles.text}>falta {seconds} segundos</Text>}
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'rgb(33, 35, 36)',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: "#fff"
-  },
-  text2: {
-    color: "red"
-  }
-});
-
+*/
