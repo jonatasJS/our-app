8a87995ba6bc1f8c65da4a61523762bd38bdd0de
jonatasJS <soaresjonatas398@gmail.com>
2022-08-30 14:57:52 -0300

    auhsdvasvbdjasd

--- App.tsx
+++ App.tsx
@@ -1,52 +1,106 @@
-import React, { useEffect, useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View, AsyncStorage, Alert } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Image, Text, View, Alert, PermissionsAndroid } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+// import * as Notifications from 'expo-notifications';
 
 export default function App() {
-  const [copos, setCopos] = useState(Number(AsyncStorage.getItem('copos')) || 0);
+  const [copos, setCopos] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    setCopos(Number(AsyncStorage.getItem('copos')) || 0);
-  }, [])
+    setTimeout(async () => {
+      setCopos(Number(await AsyncStorage.getItem("copos")) || 0);
+    }, 1);
+    setTimeout(() => {
+      setIsLoading(false); // seta o loading para false
+    }, 3000);
+  }, []);
 
   async function addCopo() {
     try {
       setCopos(copos + 1);
-      await AsyncStorage.setItem('copos', String(copos + 1));
+      await AsyncStorage.setItem("copos", `${copos + 1}`, () => '');
     } catch {
-      Alert.alert('Erro', 'Erro ao adicionar copo');
+      Alert.alert("Erro", "Erro ao adicionar copo");
+    }
+  }
+
+  async function remoCopos() {
+    try {
+      setCopos(0);
+      await AsyncStorage.setItem("copos", `${0}`, () => '');
+    } catch {
+      Alert.alert("Erro", "Erro ao remover copos");
     }
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Bebi "{copos}" copos de agua.</Text>
-      <Text
-        style={styles.button}
-        accessibilityLabel="Beber"
-        onPress={addCopo}
-      >{"Beber"}</Text>
-      <Text
-        style={styles.button}
-        accessibilityLabel="Limpar"
-        onPress={() => setCopos(0)}
-      >Zerar</Text>
-      <StatusBar style="light" />
-    </View>
+    <>
+      {isLoading && (
+        <View style={styles.loading}>
+          <Image source={require("./assets/splash-animate.gif")} />
+          <Text style={styles.loadingText}>Carregando...</Text>
+        </View>
+      )}
+      {!isLoading && (
+        <View style={styles.container}>
+          <Text style={styles.text}>Bebi "{copos}" copos de agua.</Text>
+          <Text
+            style={styles.button}
+            accessibilityLabel="Beber"
+            onPress={addCopo}
+          >
+            {"Beber"}
+          </Text>
+          <Text
+            style={styles.button}
+            accessibilityLabel="Limpar"
+            onPress={remoCopos}
+          >
+            Zerar
+          </Text>
+          {/* <Text
+            style={styles.button}
+            accessibilityLabel="Limpar"
+            onPress={async () => {
+              console.log("Sobre", await AsyncStorage.getItem("copos"))
+              console.log("Sobre", copos)
+              Alert.alert("Sobre", "Sobre o app");
+            }}
+          >
+            TESTE
+          </Text> */}
+          <StatusBar style="light" />
+        </View>
+      )}
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#2d3437',
-    alignItems: 'center',
-    justifyContent: 'center',
-    display: 'flex'
+    backgroundColor: "#1E3B81",
+    alignItems: "center",
+    justifyContent: "center",
+    display: "flex",
+  },
+  loading: {
+    flex: 1,
+    backgroundColor: "#1E3B81",
+    alignItems: "center",
+    justifyContent: "center",
+    display: "flex",
+  },
+  loadingText: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
   },
   text: {
     fontSize: 30,
-    color: "#fff"
+    color: "#fff",
   },
   button: {
     fontSize: 30,
@@ -56,11 +110,11 @@ const styles = StyleSheet.create({
     paddingLeft: 40,
     paddingRight: 40,
     color: "#fff",
-    alignItems: 'center',
-    justifyContent: 'center',
-    display: 'flex',
-    backgroundColor: '#1f9ace'
-  }
+    alignItems: "center",
+    justifyContent: "center",
+    display: "flex",
+    backgroundColor: "#1f9ace",
+  },
 });
 
 /*
