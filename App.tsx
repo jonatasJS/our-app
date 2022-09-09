4798a48f248f78589e3a2d59b148dea40a290985
jonatasJS <soaresjonatas398@gmail.com>
2022-09-09 01:48:56 -0300

    asdasdasdsd

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef, useMemo } from "react";
 import {
   StyleSheet,
   Text,
@@ -6,6 +6,8 @@ import {
   SafeAreaView,
   TouchableOpacity,
   Animated,
+  Alert,
+  Easing,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import ConfettiCannon from "react-native-confetti-cannon";
@@ -14,8 +16,10 @@ import AnimateNumber from "react-native-animate-number";
 import * as Notifications from "expo-notifications";
 import * as Update from "expo-updates";
 import Modal from "react-native-modal";
+import moment, { locale } from "moment";
 
 import { AddRemoveButton } from "./components/AddRemoveButton";
+import { NewLineKind } from "typescript";
 
 const amounts = [200, 250, 500];
 
@@ -24,7 +28,7 @@ const storeData = async (value, key = "@amount") => {
   try {
     await AsyncStorage.setItem(key, value);
   } catch (e) {
-    console.log(e);
+    Alert.alert("Erro ao salvar dados");
   }
 };
 
@@ -36,18 +40,13 @@ const getData = async (key, setValue) => {
     }
   } catch (e) {
     // error reading value
-    console.log(e);
+    Alert.alert("Erro ao ler dados");
   }
-};
-
-const renderConfetti = () => {
-  return <ConfettiCannon count={100} origin={{ x: 0, y: 0 }} fadeOut={true} />;
 };
 
 // Notifications
 async function scheduleNotification() {
   await Notifications.requestPermissionsAsync().then(async (permission) => {
-    console.log("permission", permission);
     await Notifications.scheduleNotificationAsync({
       content: {
         title: "Beba água!",
@@ -60,8 +59,6 @@ async function scheduleNotification() {
         // seconds: 10
         seconds: 1800,
       },
-    }).then((notification) => {
-      console.log("notification", notification);
     });
   });
 }
@@ -76,6 +73,23 @@ export default function App() {
   const [showModal, setShowModal] = useState(false);
   const [primaryColor, setPrimaryColor] = useState("#1ca3ec");
   const [isNotitications, setIdNotifications] = useState(false);
+  const [momentNow, setMomentNow] = useState(moment().format("HH:mm"));
+  const [dateNow, setDatetNow] = useState(moment().format("DD/MM"));
+  const [weekday, setEeekday] = useState(moment().format("ddd"));
+  const [period, setPeriod] = useState("");
+  const [isShowUnderline, setIsShowUnderline] = useState(false);
+  const today = new Date().getDate();
+  
+  const renderConfetti = () => {
+    return (
+      <ConfettiCannon
+        count={100}
+        origin={{ x: 0, y: 0 }}
+        onAnimationEnd={() => setShowConfetti(false)}
+        fadeOut={true}
+      />
+    );
+  };
 
   // Progress Bar Animation
   const barHeight = useRef(new Animated.Value(0)).current;
@@ -87,7 +101,8 @@ export default function App() {
   useEffect(() => {
     getData("@day", setDay);
     getData("@amount", setWaterDrank);
-    getData("@goal", setWaterGoal);
+    getData("@goal", setWaterGoal); // Se o dia de hoje for diferente do dia que já está salvo no async storage, então resetar o valor de agua bebida
+    periodAmimate();
     (async () => {
       await Update.checkForUpdateAsync().then(async (update) => {
         if (update.isAvailable) {
@@ -96,7 +111,48 @@ export default function App() {
           });
         }
       });
+      await getData("@day", (day) => {
+        if (day !== today) {
+          storeData(today.toString(), "@day");
+          storeData("0", "@amount");
+          setWaterDrank(0);
+        }
+      });
+      await getData("@notification", (notification) => {
+        Alert.alert("notification", notification);
+        if (notification === "true") {
+          setIdNotifications(true);
+        } else {
+          setIdNotifications(false);
+        }
+      });
     })();
+    
+    setInterval(() => {
+      setMomentNow(moment().format("HH:mm"));
+      setDatetNow(moment().format("DD/MM"));
+      setEeekday(moment().format("ddd"));
+      // setIsShowUnderline(!isShowUnderline);
+      if (moment().format("HH:mm") === "00:00") {
+        storeData("0", "@amount");
+        storeData(today.toString(), "@day");
+        setWaterDrank(0);
+      }
+      // function to check if it is morning, afternoon or evening
+      if (
+        Number(moment().format("HH")) < 12 &&
+        Number(moment().format("HH")) >= 6
+      ) {
+        setPeriod("Bom dia!");
+      } else if (
+        Number(moment().format("HH")) >= 12 &&
+        Number(moment().format("HH")) < 18
+      ) {
+        setPeriod("Boa tarde!");
+      } else {
+        setPeriod("Boa noite!");
+      }
+    }, 1000);
   }, []);
 
   useEffect(() => {
@@ -105,16 +161,6 @@ export default function App() {
       toValue: fillingPercentage / 3,
       useNativeDriver: false,
     }).start();
-
-    // Se o dia de hoje for diferente do dia que já está salvo no async storage, então resetar o valor de agua bebida
-    const today = new Date().getDate();
-    getData("@day", (day) => {
-      if (day !== today) {
-        storeData(today.toString(), "@day");
-        storeData("0", "@amount");
-        setWaterDrank(0);
-      }
-    });
   }, [fillingPercentage]);
 
   // End of Progress Bar Animation
@@ -229,6 +275,18 @@ export default function App() {
     notificationText: { color: "white", fontWeight: "500", fontSize: 16 },
   });
 
+  const periodRef = useRef(new Animated.Value(-60)).current;
+
+  const periodAmimate = () => {
+    Animated.timing(periodRef, {
+      toValue: 0,
+      delay: 1000,
+      duration: 1000,
+      useNativeDriver: true,
+      easing: Easing.bounce,
+    }).start();
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       {showConfetti && renderConfetti()}
@@ -269,6 +327,7 @@ export default function App() {
                       setShowModal(false);
                       setPrimaryColor("#25f321");
                       setIdNotifications(true);
+                      await storeData("true", "@notification");
                     }}
                   >
                     <Text style={styles.buttonText}>Sim</Text>
@@ -284,6 +343,8 @@ export default function App() {
                       await Notifications.cancelAllScheduledNotificationsAsync();
                       setShowModal(false);
                       setPrimaryColor("#1E90FF");
+                      setIdNotifications(false);
+                      await storeData("false", "@notification");
                     }}
                   >
                     <Text style={styles.buttonText}>Não</Text>
@@ -304,6 +365,7 @@ export default function App() {
                       setShowModal(false);
                       setPrimaryColor("#1E90FF");
                       setIdNotifications(false);
+                      await storeData("false", "@notification");
                     }}
                   >
                     <Text style={styles.buttonText}>Sim</Text>
@@ -318,6 +380,8 @@ export default function App() {
                     onPress={async () => {
                       setShowModal(false);
                       setPrimaryColor("#25f321");
+                      setIdNotifications(true);
+                      await storeData("true", "@notification");
                     }}
                   >
                     <Text style={styles.buttonText}>Não</Text>
@@ -329,8 +393,96 @@ export default function App() {
         </View>
       </Modal>
 
+      {/* Relogio em tempo real */}
+      <View
+        style={{
+          alignItems: "center",
+          justifyContent: "space-evenly",
+          flexDirection: "row",
+          width: "70%",
+          height: "5%",
+          marginBottom: -90,
+          marginTop: 10,
+          backgroundColor: "#fff",
+          borderRadius: 20,
+          shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          },
+          zIndex: 2,
+          shadowOpacity: 0.25,
+          shadowRadius: 3.84,
+          elevation: 5,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 30,
+            fontWeight: "bold",
+            // borderColor: isShowUnderline ? "#1e8fff4a" : "#00000000",
+            // borderWidth: 2,
+            // borderRadius: 10,
+            // paddingTop: 2,
+            // paddingBottom: 2,
+            // paddingLeft: 7,
+            // paddingRight: 5,
+          }}
+        >
+          {momentNow}
+        </Text>
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+          <Text>{dateNow} </Text>
+          {weekday === "Sun"
+            ? "Dom"
+            : weekday === "Mon"
+            ? "Seg"
+            : weekday === "Tue"
+            ? "Ter"
+            : weekday === "Wed"
+            ? "Qua"
+            : weekday === "Thu"
+            ? "Qui"
+            : weekday === "Fri"
+            ? "Sex"
+            : "Sáb"}
+        </Text>
+      </View>
+      <Animated.View
+        style={{
+          width: "50%",
+          height: "3.5%",
+          justifyContent: "center",
+          alignItems: "center",
+          position: "relative",
+          backgroundColor: "#fff",
+          borderRadius: 10,
+          marginTop: -45,
+          translateY: periodRef,
+          marginBottom: -120,
+          zIndex: 1,
+          shadowColor: "#000",
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          },
+          shadowOpacity: 0.25,
+          shadowRadius: 3.84,
+          elevation: 5,
+        }}
+      >
+        <Text>{period}</Text>
+      </Animated.View>
+
       {/* Water Goal */}
-      <View style={styles.waterGoalContainer}>
+      <View
+        style={[
+          styles.waterGoalContainer,
+          {
+            marginBottom: -50,
+          },
+        ]}
+      >
         <Text
           style={[
             styles.blueText,
